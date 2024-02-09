d5dd66ed3dd2a1166fd99a606943d480b7aa0515
Satria Bahari <satriaabaharii@gmail.com>
2024-02-09 18:12:00 +0700

    feat: add home page

---
+++ modules/home/components/Home.tsx
@@ -0,0 +1,11 @@
+import Breakline from "@/common/components/elements/Breakline";
+import Introduction from "./Introduction";
+
+export default function Home() {
+  return (
+    <>
+      <Introduction />
+      <Breakline />
+    </>
+  );
+}
---
+++ modules/home/components/Introduction.tsx
@@ -0,0 +1,25 @@
+export default function Introduction() {
+  return (
+    <section className="space-y-2">
+      <div className="text-3xl font-medium">
+        <h1>Hi, I&apos;m Satria Bahari</h1>
+      </div>
+
+      <div className="space-y-4">
+        <ul className="ml-5 flex list-disc gap-x-8">
+          <li>
+            Based in Jambi, Indonesia <span>🇮🇩</span>
+          </li>
+          <li>Remote worker</li>
+        </ul>
+        <p>
+          Passionate and seasoned Software Engineer with a strong focus on
+          frontend development. Proficient in TypeScript and well-versed in all
+          aspects of web technologies. Collaborative team player dedicated to
+          delivering efficient, scalable, and visually appealing web
+          applications.
+        </p>
+      </div>
+    </section>
+  );
+}
