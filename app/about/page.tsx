5351f1a820f9dcc29103d6c0b3a2ef4cdf5d353f
Satria Bahari <satriaabaharii@gmail.com>
2024-02-14 13:57:58 +0700

    refactor: add container in some page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,15 @@
+import Container from "@/common/components/elements/Container";
 import PageHeading from "@/common/components/elements/PageHeading";
 import About from "@/modules/about";
 
 const PAGE_TITLE = "About";
-const PAGE_DESCRIPTION = 'A short story of me'
+const PAGE_DESCRIPTION = "A short story of me";
 
 export default function AboutPage() {
   return (
-    <>
-      <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION}/>
+    <Container>
+      <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
       <About />
-    </>
+    </Container>
   );
 }
