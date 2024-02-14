49ff382207fad79da147997efb8f511425e8c353
Satria Bahari <satriaabaharii@gmail.com>
2024-02-14 15:10:46 +0700

    refactor: add some code in sidebar

--- common/components/layouts/index.tsx
+++ common/components/layouts/index.tsx
@@ -6,9 +6,11 @@ type LayoutsProps = {
 
 export default function Layouts({ children }: LayoutsProps) {
   return (
-    <div className="flex w-full">
-      <Sidebar />
-      <main className="w-4/5 p-4">{children}</main>
+    <div className="px-12">
+      <div className="flex gap-5 py-4">
+        <Sidebar />
+        <main className="w-4/5">{children}</main>
+      </div>
     </div>
   );
 }
