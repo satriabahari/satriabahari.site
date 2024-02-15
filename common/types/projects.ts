163ae66ae0b0ed1c649c8408536c8f768985881c
Satria Bahari <satriaabaharii@gmail.com>
2024-02-15 14:00:59 +0700

    refactor: setup project page

---
+++ common/types/projects.ts
@@ -0,0 +1,17 @@
+export type ProjectItem = {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  image: string;
+  link_demo?: string | null;
+  link_github?: string | null;
+  stacks: string[];
+  content?: string | null;
+  is_show: boolean;
+  is_featured: boolean;
+};
+
+export type ProjectItemProps = {
+  projects: ProjectItem[];
+}
--- modules/projects/components/ProjectCard.tsx
+++ modules/projects/components/ProjectCard.tsx
@@ -1,5 +1,31 @@
-export default function ProjectCard() {
+import Card from "@/common/components/elements/Card";
+import { SKILLS } from "@/common/constant/skills";
+import { ProjectItem } from "@/common/types/projects";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProjectCard({
+  title,
+  slug,
+  description,
+  image,
+  stacks,
+  is_featured,
+}: ProjectItem) {
   return (
-    <div>ProjectCard</div>
-  )
+    <Link href={`/projects/${slug}`}>
+      <Card>
+        {/* <Image src={image} alt={title} width={400} height={200} /> */}
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          {/* <div>
+            {stacks.map((stack, index) => (
+              <div key={index}>{SKILLS[stack]}</div>
+            ))}
+          </div> */}
+        </div>
+      </Card>
+    </Link>
+  );
 }
