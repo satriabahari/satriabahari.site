bca8005c203e092cb3796e5c692266cfb8fba44f
Satria Bahari <satriaabaharii@gmail.com>
2024-02-16 00:12:44 +0700

    feat(project detail): add project detail feature

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import BackButton from "@/common/components/elements/BackButton";
+import Container from "@/common/components/elements/Container";
+import PageHeading from "@/common/components/elements/PageHeading";
+import ProjectDetail from "@/modules/projects/components/ProjectDetail";
+import axios from "axios";
+
+type ProjectDetailPageProps = {
+  params: { slug: string };
+};
+
+export default async function ProjectDetailPage({
+  params,
+}: ProjectDetailPageProps) {
+  const response = await axios.get("http://localhost:3000/api/projects");
+  const projects = response.data.data;
+
+  const project = projects.find((item: any) => item.slug === params.slug);
+
+  const PAGE_TITLE = project.title;
+  const PAGE_DESCRIPTION = project.description;
+  return (
+    <Container>
+      <BackButton url="/projects" />
+      <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+      <ProjectDetail {...project} />
+    </Container>
+  );
+}
---
+++ modules/projects/components/ProjectDetail.tsx
@@ -0,0 +1,36 @@
+import { SKILLS } from "@/common/constant/skills";
+import { ProjectItem } from "@/common/types/projects";
+import Image from "next/image";
+import React from "react";
+import ProjectLink from "./ProjectLink";
+
+export default function ProjectDetail({
+  title,
+  image,
+  stacks,
+  link_demo,
+  link_github,
+  content,
+}: ProjectItem) {
+  const stacksArray = JSON.parse(stacks);
+  return (
+    <div className="space-y-8 overflow-hidden">
+      <div className="flex flex-col items-center justify-between lg:flex-row">
+        <div className="flex flex-wrap items-center gap-2">
+          <span className="mb-1 text-sm">Tech Stack: </span>
+          <div className="flex items-center gap-3">
+            {stacksArray.map((stack: string, index: number) => (
+              <div key={index}>{SKILLS[stack]}</div>
+            ))}
+          </div>
+        </div>
+        <ProjectLink
+          title={title}
+          link_demo={link_demo || ""}
+          link_github={link_github || ""}
+        />
+      </div>
+      <Image src={image} alt={title} width={1000} height={400} />
+    </div>
+  );
+}
