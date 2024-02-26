17faecdf2cc09914c57efa93dc1dc38fb5f780b9
Satria Bahari <satriaabaharii@gmail.com>
2024-02-26 22:59:26 +0700

    style: add responsive in mobile

--- modules/projects/components/Projects.tsx
+++ modules/projects/components/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 
 export default function Projects({ projects }: ProjectItemProps) {
   return (
-    <section className="grid grid-cols-2 gap-12">
+    <section className="grid grid-cols-1 gap-12 lg:grid-cols-2">
       {projects?.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
