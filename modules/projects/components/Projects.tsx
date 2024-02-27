5357582a1a3cc8b00b784017d7574f51bff82d12
Satria Bahari <satriaabaharii@gmail.com>
2024-02-27 17:22:24 +0700

    refactor: add empty data component

--- modules/projects/components/Projects.tsx
+++ modules/projects/components/Projects.tsx
@@ -1,11 +1,18 @@
 import { ProjectItemProps } from "@/common/types/projects";
 
 import ProjectCard from "./ProjectCard";
+import EmptyState from "@/common/components/elements/EmptyState";
 
 export default function Projects({ projects }: ProjectItemProps) {
+  const filteredProjects = projects.filter((project) => project?.is_show);
+
+  if (filteredProjects.length === 0) {
+    return <EmptyState message="No Data" />;
+  }
+
   return (
     <section className="grid grid-cols-1 gap-12 lg:grid-cols-2">
-      {projects?.map((project, index) => (
+      {filteredProjects?.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
     </section>
