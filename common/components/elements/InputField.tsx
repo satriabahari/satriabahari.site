a842f9c970b43b03a76d0d158ceecb8eb8c0a363
Satria Bahari <satriaabaharii@gmail.com>
2024-02-27 22:13:49 +0700

    refactor: change some code

--- common/components/elements/InputField.tsx
+++ common/components/elements/InputField.tsx
@@ -1,22 +1,57 @@
-export default function InputField({
+import {
+  FieldErrors,
+  FieldValues,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
+
+type RadioInputProps<TFormValue extends FieldValues> = {
+  register: UseFormRegister<TFormValue>;
+  name: Path<TFormValue>;
+  error: FieldErrors;
+  rule?: RegisterOptions;
+  isTextArea?: boolean;
+  placeholder?: string;
+  rows?: number;
+};
+export default function InputField<TFormValue extends FieldValues>({
+  name,
+  rule,
+  error,
   isTextArea = false,
   placeholder = "",
   rows = 2,
-}) {
+  register,
+}: RadioInputProps<TFormValue>) {
+  const renderPlaceholder =
+    placeholder || name.charAt(0).toUpperCase() + name.slice(1);
   return (
     <div className="w-full space-y-2">
       {isTextArea ? (
         <textarea
           rows={rows}
-          placeholder={placeholder}
+          placeholder={renderPlaceholder}
+          {...register(name, rule)}
           className="w-full rounded-lg bg-neutral-50 p-2 outline outline-neutral-300 focus:outline-neutral-400 dark:bg-neutral-900 dark:outline-neutral-700"
         ></textarea>
       ) : (
         <input
           type="text"
-          placeholder={placeholder}
+          placeholder={renderPlaceholder}
+          {...register(name, rule)}
           className="w-full rounded-lg bg-neutral-50 p-2 outline outline-neutral-300 focus:outline-neutral-400 dark:bg-neutral-900 dark:outline-neutral-700"
         />
+      )}{" "}
+      {error[name]?.type === "required" && (
+        <p role="alert" className="text-[10px] text-red-400">
+          *{name} is required
+        </p>
+      )}
+      {error[name]?.type === "pattern" && (
+        <p role="alert" className="text-[10px] text-red-400">
+          *{String(error[name]?.message)}
+        </p>
       )}
     </div>
   );
--- modules/contact/components/ContactList.tsx
+++ modules/contact/components/ContactList.tsx
@@ -2,11 +2,12 @@ import { SOCIAL_MEDIA } from "@/common/constant/socialMedia";
 import ContactCard from "./ContactCard";
 
 export default function ContactList() {
+  const filteredSocialMedia = SOCIAL_MEDIA?.filter((social) => social?.isShow);
   return (
-    <div className="space-y-4">
+    <div className="flex flex-col space-y-4">
       <h2>Find me on social media</h2>
-      <div className="flex gap-x-4">
-        {SOCIAL_MEDIA.map((media) => (
+      <div className="flex flex-wrap gap-4">
+        {filteredSocialMedia.map((media) => (
           <ContactCard key={media.title} {...media} />
         ))}
       </div>
