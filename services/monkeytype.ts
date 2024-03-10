803a3a9852f04a8a391cdc60e2f254ef33e39016
Satria Bahari <satriaabaharii@gmail.com>
2024-03-10 21:49:38 +0700

    refactor: change some code

--- services/monkeytype.ts
+++ services/monkeytype.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+type getMonkeytypeLeaderboardProps = {
+  language: string;
+  mode: string;
+  mode2: number;
+};
+
 export async function getMonkeytypeData() {
   const USER_NAME = process.env.NEXT_PUBLIC_USER_NAME;
   const MONKEYTYPE_API = `https://api.monkeytype.com/users/${USER_NAME}/profile`;
@@ -11,11 +17,12 @@ export async function getMonkeytypeData() {
   }
 }
 
-export async function getMonkeytypeLeaderboard() {
+export async function getMonkeytypeLeaderboard({
+  language,
+  mode,
+  mode2,
+}: getMonkeytypeLeaderboardProps) {
   const API_KEY = process.env.MONKEYTYPE_API_KEY;
-  const language = "english";
-  const mode = "time";
-  const mode2 = "60";
   const MONKEYTYPE_API = `https://api.monkeytype.com/leaderboards/rank?&language=${language}&mode=${mode}&mode2=${mode2}`;
   try {
     const response = await axios.get(MONKEYTYPE_API, {
