db6b8e9fae07fc7c6fb0075c99b9181e48f30243
Satria Bahari <satriaabaharii@gmail.com>
2024-03-30 11:30:31 +0700

    fix: fix logic for fetching data monkeytype

--- services/monkeytype.ts
+++ services/monkeytype.ts
@@ -1,37 +1,19 @@
+import { MONKEYTYPE_ACCOUNT } from "@/common/constant/monkeytype";
 import axios from "axios";
 
-type getMonkeytypeLeaderboardProps = {
-  language: string;
-  mode: string;
-  mode2: number;
-};
+const { username } = MONKEYTYPE_ACCOUNT;
 
-export async function getMonkeytypeData() {
-  const USER_NAME = process.env.NEXT_PUBLIC_USER_NAME;
-  const MONKEYTYPE_API = `https://api.monkeytype.com/users/${USER_NAME}/profile`;
-  try {
-    const response = await axios.get(MONKEYTYPE_API);
-    return response.data.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const USER_ENDPOINT = `https://api.monkeytype.com/users/${username}/profile`;
 
-export async function getMonkeytypeLeaderboard({
-  language,
-  mode,
-  mode2,
-}: getMonkeytypeLeaderboardProps) {
-  const API_KEY = process.env.MONKEYTYPE_API_KEY;
-  const MONKEYTYPE_API = `https://api.monkeytype.com/leaderboards/rank?&language=${language}&mode=${mode}&mode2=${mode2}`;
-  try {
-    const response = await axios.get(MONKEYTYPE_API, {
-      headers: {
-        Authorization: `ApeKey ${API_KEY}`,
-      },
-    });
-    return response.data.data;
-  } catch (error) {
-    console.log(error);
+export async function getUserData() {
+  const response = await axios.get(USER_ENDPOINT);
+
+  const status = response.status;
+  const responseJson = response.data;
+
+  if (status > 400) {
+    return { status, data: {} };
   }
+
+  return { status, data: responseJson.data };
 }
