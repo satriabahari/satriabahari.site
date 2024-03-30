3ca074fb50d684f03e2cfc4ec46ffaf9ce9b897b
Satria Bahari <satriaabaharii@gmail.com>
2024-03-30 11:40:22 +0700

    refactor: change some code on project API route

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -5,8 +5,11 @@ import prisma from "@/common/libs/prisma";
 export const GET = async () => {
   try {
     const data = await prisma.projects.findMany();
-    return NextResponse.json({ status: true, data }, { status: 200 });
+    return NextResponse.json(data, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ status: false, error }, { status: 400 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
   }
 };
