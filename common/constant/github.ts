908ec96cb2aa8b0c54d8253323300d3206f0d53e
Satria Bahari <satriaabaharii@gmail.com>
2024-03-30 11:38:32 +0700

    refactor: change some code and add some property on codewars

--- common/constant/github.ts
+++ common/constant/github.ts
@@ -1,33 +1,8 @@
-export const GITHUB_API_BASE_URL = 'https://api.github.com/graphql'
-
 export const GITHUB_ACCOUNTS = {
-  username: 'satriabahari',
-  token: process.env.GITHUB_TOKEN,
-  endpoint: '/api/github?type=personal',
-  type: 'personal',
-  is_active: true
-}
-
-export const GITHUB_USER_QUERY = `query($username: String!) {
-    user(login: $username) {
-      contributionsCollection {
-        contributionCalendar {
-          colors
-          totalContributions
-          months {
-            firstDay
-            name
-            totalWeeks
-          }
-          weeks {
-            contributionDays {
-              color
-              contributionCount
-              date
-            }
-            firstDay
-          }
-        }
-      }
-    }
-  }`
+  username: "satriabahari",
+  token: process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
+  endpoint: "/api/github",
+  type: "personal",
+  github_url: "https://github.com/satriabahari",
+  is_active: true,
+};
