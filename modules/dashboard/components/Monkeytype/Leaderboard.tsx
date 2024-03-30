d31e5d0d24074e74b0538e07a72d29e6c7e338ee
Satria Bahari <satriaabaharii@gmail.com>
2024-03-30 13:01:44 +0700

    refactor: change some code on leaderboard monkeytype

--- modules/dashboard/components/Monkeytype/Leaderboard.tsx
+++ modules/dashboard/components/Monkeytype/Leaderboard.tsx
@@ -1,36 +1,36 @@
 import { convertToOrdinal } from "@/common/helpers";
-import { MonkeytypeLeaderboard } from "@/common/types/monkeytype";
-import clsxm from "@/common/libs/clsxm";
-
-import OverviewItem from "./OverviewItem";
+import { MonkeytypeData } from "@/common/types/monkeytype";
 
 type LeaderboardProps = {
-  datas: MonkeytypeLeaderboard[];
-  label: string;
-  className?: string;
+  data: MonkeytypeData;
 };
 
-export default function Leaderboard({
-  label,
-  datas,
-  className,
-}: LeaderboardProps) {
+type ItemProps = {
+  label: string;
+  value: string;
+};
+
+const Item = ({ label, value }: ItemProps) => {
   return (
-    <div
-      className={clsxm(
-        "flex flex-col items-center justify-between gap-y-1 rounded-xl border border-neutral-200 bg-neutral-100 px-4 py-3 dark:border-neutral-800 dark:bg-neutral-800 sm:flex-row",
-        className,
-      )}
-    >
+    <div className="flex items-center gap-3">
+      <span className="text-sm dark:text-neutral-400">{label} Seconds</span>
+      <span>{value}</span>
+    </div>
+  );
+};
+
+export default function Leaderboard({ data }: LeaderboardProps) {
+  const datas = Object.values(data.allTimeLbs.time);
+  return (
+    <div className="flex flex-col items-center justify-between gap-y-1 rounded-xl border border-neutral-200 bg-neutral-100 px-4 py-3 dark:border-neutral-800 dark:bg-neutral-800 sm:flex-row">
       <span className="text-sm text-neutral-600 dark:text-neutral-400">
-        {label}
+        All-Time English Leaderboards
       </span>
       {datas.map((data, index) => (
-        <OverviewItem
+        <Item
           key={index}
-          label={index === 0 ? "15 seconds" : "60 seconds"}
-          value={data?.rank ? convertToOrdinal(data.rank) : "-"}
-          isFlex
+          label={index == 0 ? "15" : "60"}
+          value={convertToOrdinal(data.english)}
         />
       ))}
     </div>
