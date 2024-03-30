b856982c5b7ed71fe3aa5fdb181e59ea62acefd2
Satria Bahari <satriaabaharii@gmail.com>
2024-03-30 12:59:47 +0700

    refactor: implement clean code on dahboard

--- modules/dashboard/components/Dashboard.tsx
+++ modules/dashboard/components/Dashboard.tsx
@@ -1,43 +1,23 @@
-import Breakline from "@/common/components/elements/Breakline";
-import { CodewarsData } from "@/common/types/codewars";
-import {
-  MonkeytypeData,
-  MonkeytypeLeaderboard,
-} from "@/common/types/monkeytype";
-
-import Contributions from "./Contributions";
 import Codewars from "./Codewars";
 import Monkeytype from "./Monkeytype";
 import CodingActive from "./CodingActive";
+import Contributions from "./Contributions";
 
-type DashboardProps = {
-  githubData: any;
-  codewarsData: CodewarsData;
-  monkeytypeDataProfile: MonkeytypeData;
-  monkeytypeDataTime60Leaderboard: MonkeytypeLeaderboard;
-  monkeytypeDataTime15Leaderboard: MonkeytypeLeaderboard;
-};
+import Breakline from "@/common/components/elements/Breakline";
+import { GITHUB_ACCOUNTS } from "@/common/constant/github";
+import { MONKEYTYPE_ACCOUNT } from "@/common/constant/monkeytype";
+import { CODEWARS_ACCOUNT } from "@/common/constant/codewars";
 
-export default function Dashboard({
-  githubData,
-  codewarsData,
-  monkeytypeDataProfile,
-  monkeytypeDataTime60Leaderboard,
-  monkeytypeDataTime15Leaderboard,
-}: DashboardProps) {
+export default function Dashboard() {
   return (
     <>
-      <Contributions githubData={githubData} />
+      <Contributions endpoint={GITHUB_ACCOUNTS.endpoint} />
       <Breakline className="my-8" />
-      {/* <CodingActive /> */}
+      <CodingActive />
       <Breakline className="my-8" />
-      <Codewars codewarsData={codewarsData} />
+      <Codewars endpoint={CODEWARS_ACCOUNT.endpoint} />
       <Breakline className="my-8" />
-      {/* <Monkeytype
-        monkeytypeDataProfile={monkeytypeDataProfile}
-        monkeytypeDataTime60Leaderboard={monkeytypeDataTime60Leaderboard}
-        monkeytypeDataTime15Leaderboard={monkeytypeDataTime15Leaderboard}
-      /> */}
+      <Monkeytype endpoint={MONKEYTYPE_ACCOUNT.endpoint} />
     </>
   );
 }
