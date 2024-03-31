77f4821d448f512114a416ed99a4a02798d4e2d9
Satria Bahari <satriaabaharii@gmail.com>
2024-03-31 11:42:49 +0700

    style: delete padding style

--- modules/dashboard/components/Monkeytype/Overview.tsx
+++ modules/dashboard/components/Monkeytype/Overview.tsx
@@ -16,7 +16,7 @@ export default function Overview({ data }: OverviewProps) {
   };
 
   return (
-    <div className="grid grid-cols-1 gap-3 py-2 sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
       <OverviewItem data={data.personalBests.time} type="time" />
       <OverviewItem data={data.personalBests.words} type="words" />
     </div>
