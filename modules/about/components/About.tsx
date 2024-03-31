0f126d66346b63f19c196c58ebde34cea3af7dc2
Satria Bahari <satriaabaharii@gmail.com>
2024-03-31 13:21:05 +0700

    refactor: delete skill list on about page

--- modules/about/components/About.tsx
+++ modules/about/components/About.tsx
@@ -3,14 +3,12 @@ import Breakline from "@/common/components/elements/Breakline";
 import Story from "./Story";
 import CareerList from "./CareerList";
 import EducationList from "./EducationList";
-import SkillList from "./SkillList";
+import SkillList from "../../home/components/SkillList";
 
 export default function About() {
   return (
     <>
       <Story />
-      <Breakline className="my-8" />
-      <SkillList />
       <Breakline className="my-8" />
       <CareerList />
       <Breakline className="my-8" />
