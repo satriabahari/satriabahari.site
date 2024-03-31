dad64c1d1283007f49474ee1de4e2c1dd4416c3a
Satria Bahari <satriaabaharii@gmail.com>
2024-03-31 13:21:17 +0700

    refactor: add skill list on home page

--- modules/home/components/Home.tsx
+++ modules/home/components/Home.tsx
@@ -1,12 +1,14 @@
 import Breakline from "@/common/components/elements/Breakline";
 
 import Introduction from "./Introduction";
+import SkillList from "./SkillList";
 
 export default function Home() {
   return (
     <>
       <Introduction />
       <Breakline className="my-8" />
+      <SkillList />
     </>
   );
 }
