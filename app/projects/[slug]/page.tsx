47c0487127dce28b79920530c2f8c3fe0dae0245
Satria Bahari <satriaabaharii@gmail.com>
2024-03-31 13:07:18 +0700

    refactor: change data fetch logic

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,33 +1,31 @@
-// import axios from "axios";
+import BackButton from "@/common/components/elements/BackButton";
+import Container from "@/common/components/elements/Container";
+import PageHeading from "@/common/components/elements/PageHeading";
+import ProjectDetail from "@/modules/projects/components/ProjectDetail";
+import prisma from "@/common/libs/prisma";
 
-// import BackButton from "@/common/components/elements/BackButton";
-// import Container from "@/common/components/elements/Container";
-// import PageHeading from "@/common/components/elements/PageHeading";
-// import ProjectDetail from "@/modules/projects/components/ProjectDetail";
+type ProjectDetailPageProps = {
+  params: { slug: string };
+};
 
-// type ProjectDetailPageProps = {
-//   params: { slug: string };
-// };
+export default async function ProjectDetailPage({
+  params,
+}: ProjectDetailPageProps) {
+  const response = await prisma.projects.findUnique({
+    where: {
+      slug: String(params?.slug),
+    },
+  });
 
-// export default async function ProjectDetailPage({
-//   params,
-// }: ProjectDetailPageProps) {
-//   const response = await axios.get("http://localhost:3000/api/projects");
-//   const projects = response.data.data;
+  const data = JSON.parse(JSON.stringify(response));
 
-//   const project = projects.find((item: any) => item.slug === params.slug);
-
-//   const PAGE_TITLE = project.title;
-//   const PAGE_DESCRIPTION = project.description;
-//   return (
-//     <Container data-aos="fade-up">
-//       <BackButton url="/projects" />
-//       <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-//       <ProjectDetail {...project} />
-//     </Container>
-//   );
-// }
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <h1>{`My Page ${params}`}</h1>;
+  const PAGE_TITLE = data?.title;
+  const PAGE_DESCRIPTION = data?.description;
+  return (
+    <Container data-aos="fade-up">
+      <BackButton url="/projects" />
+      <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+      <ProjectDetail {...data} />
+    </Container>
+  );
 }
