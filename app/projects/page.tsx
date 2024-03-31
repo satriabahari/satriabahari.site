fd3b9ed9c0593a56e52557e5a505639ee4a3d370
Satria Bahari <satriaabaharii@gmail.com>
2024-03-31 13:06:45 +0700

    refactor: activate the projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,30 +1,16 @@
-// import axios from "axios";
+import Container from "@/common/components/elements/Container";
+import PageHeading from "@/common/components/elements/PageHeading";
+import Projects from "@/modules/projects";
 
-// import Container from "@/common/components/elements/Container";
-// import PageHeading from "@/common/components/elements/PageHeading";
-// import Projects from "@/modules/projects";
+const PAGE_TITLE = "Projects";
+const PAGE_DESCRIPTION =
+  "Several projects that I have worked on, both private and open source.";
 
-// const PAGE_TITLE = "Projects";
-// const PAGE_DESCRIPTION =
-//   "Several projects that I have worked on, both private and open source.";
-
-// export default async function ProjectsPage() {
-//   const response = await axios.get("http://localhost:3000/api/projects");
-//   const projects = response.data.data;
-//   return (
-//     <Container data-aos="fade-up">
-//       <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-//       <Projects projects={projects} />
-//       <div>test</div>
-//     </Container>
-//   );
-// }
-
-import React from 'react'
-
-export default function ProjectsPage() {
+export default async function ProjectsPage() {
   return (
-    <div>ProjectsPage</div>
-  )
+    <Container data-aos="fade-up">
+      <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+      <Projects />
+    </Container>
+  );
 }
-
