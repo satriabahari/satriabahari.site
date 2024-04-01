171015e3c2ed0a931afa32010d162b2411ab9b97
Satria Bahari <satriaabaharii@gmail.com>
2024-04-01 15:47:23 +0700

    refactor: change some code

--- modules/dashboard/components/Monkeytype/Leaderboard.tsx
+++ modules/dashboard/components/Monkeytype/Leaderboard.tsx
@@ -9,12 +9,18 @@ type LeaderboardProps = {
 type ItemProps = {
   label: string;
   value: string;
+  percent?: number;
 };
 
-const Item = ({ label, value }: ItemProps) => {
+const Item = ({ label, value, percent }: ItemProps) => {
   return (
-    <div className="flex items-center gap-3">
-      <span className="text-sm dark:text-neutral-500">{label} Seconds</span>
+    <div className="flex items-center gap-4">
+      <div className="flex flex-col items-end gap-y-0.5">
+        <span className="text-sm dark:text-neutral-500">{label} Seconds</span>
+        {percent ? (
+          <span className="text-xs dark:text-neutral-500">Top {percent}%</span>
+        ) : null}
+      </div>
       <span className="text-2xl dark:text-neutral-50">{value}</span>
     </div>
   );
@@ -23,17 +29,22 @@ const Item = ({ label, value }: ItemProps) => {
 export default function Leaderboard({ data }: LeaderboardProps) {
   const datas = Object.values(data.allTimeLbs.time) || [];
   return (
-    <Card className="flex flex-col items-center justify-between gap-y-1 px-4 py-3 sm:flex-row">
+    <Card className="flex flex-col items-center justify-between gap-y-3 px-4 py-3 sm:flex-row sm:gap-y-1">
       <span className="text-sm text-neutral-600 dark:text-neutral-500">
         All-Time English Leaderboards
       </span>
-      {datas.map((data, index) => (
-        <Item
-          key={index}
-          label={index == 0 ? "15" : "60"}
-          value={convertToOrdinal(data.english) || "-"}
-        />
-      ))}
+      {datas.map((data, index) => {
+        return (
+          <Item
+            key={index}
+            label={index == 0 ? "15" : "60"}
+            value={convertToOrdinal(data?.english?.rank) || "-"}
+            percent={Math.round(
+              (data?.english.rank / data?.english?.count) * 100,
+            )}
+          />
+        );
+      })}
     </Card>
   );
 }
