9a67058316aad9f3ce8b293ae5ee9845b134c0d1
Satria Bahari <satriaabaharii@gmail.com>
2024-04-05 14:03:35 +0700

    refactor: add clan and skills item

--- modules/dashboard/components/Codewars/Overview.tsx
+++ modules/dashboard/components/Codewars/Overview.tsx
@@ -8,21 +8,30 @@ type OverviewProps = {
 
 export default function Overview({ data }: OverviewProps) {
   return (
-    <div className="grid grid-cols-2 gap-3 py-2 sm:grid-cols-4">
-      <OverviewItem
-        label="Rank"
-        value={Math.abs(data?.ranks.overall.rank)}
-        unit="kyu"
-      />
-      <OverviewItem label="Honor" value={data?.honor} />
-      <OverviewItem
-        label="Completed"
-        value={data?.codeChallenges.totalCompleted}
-      />
-      <OverviewItem
-        label="Leaderboard"
-        value={data?.leaderboardPosition ? data?.leaderboardPosition : 0}
-      />
+    <div>
+      <div className="grid grid-cols-2 gap-3 py-2 sm:grid-cols-4">
+        <OverviewItem
+          label="Rank"
+          value={Math.abs(data?.ranks.overall.rank)}
+          unit="kyu"
+        />
+        <OverviewItem label="Honor" value={data?.honor} />
+        <OverviewItem
+          label="Completed"
+          value={data?.codeChallenges.totalCompleted}
+        />
+        <OverviewItem
+          label="Leaderboard"
+          value={data?.leaderboardPosition ? data?.leaderboardPosition : 0}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3 py-2 capitalize sm:grid-cols-2">
+        <OverviewItem label="Clan" value={data?.clan} />
+        <OverviewItem
+          label="Skills"
+          value={data?.skills?.map((skill) => skill).join(", ")}
+        />
+      </div>
     </div>
   );
 }
