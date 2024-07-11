23246b2d06678020d64a010986762026289f7209
Satria Bahari <satriaabaharii@gmail.com>
2024-07-11 08:55:52 +0700

    feat: add chat room feature

---
+++ app/[locale]/chat/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+import Container from "@/common/components/elements/Container";
+import PageHeading from "@/common/components/elements/PageHeading";
+import Contact from "@/modules/contact";
+import { METADATA } from "@/common/constant/metadata";
+import { useTranslations } from "next-intl";
+import ChatRoom from "@/modules/chat";
+
+export const metadata: Metadata = {
+  title: `Contact ${METADATA.exTitle}`,
+  description: `Contact ${METADATA.creator}`,
+  alternates: {
+    canonical: `${process.env.DOMAIN}/contact`,
+  },
+};
+
+const PAGE_TITLE = "Contact";
+const PAGE_DESCRIPTION = "Let`s get in touch";
+
+export default function ContactPage() {
+  const t = useTranslations("ChatRoomPage");
+  return (
+    <Container data-aos="fade-up">
+      <PageHeading title={t("title")} description={t("description")} />
+      <ChatRoom />
+    </Container>
+  );
+}
