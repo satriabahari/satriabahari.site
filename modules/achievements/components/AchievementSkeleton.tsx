a6a886d5350acda9c3c581ac2b964e38fc48dd26
satria bahari <satriaabaharii@gmail.com>
2024-08-02 21:41:16 +0700

    feat: add achievements feature

---
+++ modules/achievements/components/AchievementSkeleton.tsx
@@ -0,0 +1,14 @@
+const AchievementSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+      <div className="h-72 w-full animate-pulse rounded-lg bg-neutral-300 dark:bg-neutral-800" />
+    </div>
+  );
+};
+
+export default AchievementSkeleton;
