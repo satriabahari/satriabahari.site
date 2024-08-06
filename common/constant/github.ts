2cb1db670c8f61f457c280084d573f2e4e2d3962
satria bahari <satriaabaharii@gmail.com>
2024-08-06 16:16:49 +0700

    refactor: delete some file

--- common/constant/github.ts
+++
@@ -1,8 +0,0 @@
-export const GITHUB_ACCOUNTS = {
-  username: "satriabahari",
-  token: process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
-  endpoint: "/api/github",
-  type: "personal",
-  github_url: "https://github.com/satriabahari",
-  is_active: true,
-};
--- common/constant/monkeytype.ts
+++
@@ -1,7 +0,0 @@
-export const MONKEYTYPE_ACCOUNT = {
-  username: "SatriaAxel",
-  api_key: process.env.MONKEYTYPE_API_KEY,
-  endpoint: "/api/monkeytype",
-  monkeytype_url: "https://monkeytype.com/profile/SatriaAxel",
-  is_active: true,
-};
