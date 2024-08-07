5f322ea8d79f971d6fff65d7a0aff742af511ccf
satria bahari <satriaabaharii@gmail.com>
2024-08-07 14:48:26 +0700

    refactor: delete some file

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/common/utils/server";
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
-
-  return NextResponse.json(notes, { status: 200 });
-};
