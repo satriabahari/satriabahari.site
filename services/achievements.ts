337bdc2d390eaaeb263d866ef4f30f3b72d7f6c5
satria bahari <satriaabaharii@gmail.com>
2024-08-07 14:48:59 +0700

    refactor: switch from firebase to supabase

--- services/achievements.ts
+++ services/achievements.ts
@@ -1,70 +1,31 @@
-import { firestore } from "@/common/libs/firebase";
-import { AchievementItem } from "@/common/types/achievements";
-import { collection, getDocs, query, where } from "@firebase/firestore";
+import { createClient } from "@/common/utils/server";
 
-export const getAchievementsData = async () => {
-  const querySnapshot = await getDocs(collection(firestore, "achievements"));
-  const projects: AchievementItem[] = [];
-  querySnapshot.forEach((doc) => {
-    const data = { id: parseInt(doc.id), ...doc.data() } as AchievementItem;
-    projects.push(data);
-  });
-  return projects;
-};
+interface GetAchievementsDataProps {
+  category?: string;
+  search?: string;
+}
 
-export const getAchievementsDataByCategory = async (category: string) => {
-  const q = query(
-    collection(firestore, "achievements"),
-    where("category", "==", category),
-  );
-  const querySnapshot = await getDocs(q);
-  let data: AchievementItem[] = [];
-  querySnapshot.forEach((doc) => {
-    data.push({ id: parseInt(doc.id), ...doc.data() } as AchievementItem);
-  });
-  return data;
-};
+export const getAchievementsData = async ({
+  category,
+  search,
+}: GetAchievementsDataProps) => {
+  const supabase = createClient();
 
-export const getAchievementsDataByQuerySearch = async (querySearch: string) => {
-  const decodedQuery = decodeURIComponent(querySearch).toLowerCase();
-  const q = query(collection(firestore, "achievements"));
-  const querySnapshot = await getDocs(q);
+  let query = supabase.from("achievements").select();
 
-  let data: AchievementItem[] = [];
-  querySnapshot.forEach((doc) => {
-    const achievement = {
-      id: parseInt(doc.id),
-      ...doc.data(),
-    } as AchievementItem;
-    if (achievement.name.toLowerCase().includes(decodedQuery)) {
-      data.push(achievement);
-    }
-  });
+  if (category) {
+    query = query.eq("category", category);
+  }
 
-  return data;
-};
+  if (search) {
+    query = query.ilike("name", `%${search}%`);
+  }
 
-export const getAchievementsDataByCategoryAndQuery = async (
-  category: string,
-  querySearch: string,
-) => {
-  const decodedQuery = decodeURIComponent(querySearch).toLowerCase();
-  const q = query(
-    collection(firestore, "achievements"),
-    where("category", "==", category),
-  );
-  const querySnapshot = await getDocs(q);
+  const { data, error } = await query;
 
-  let data: AchievementItem[] = [];
-  querySnapshot.forEach((doc) => {
-    const achievement = {
-      id: parseInt(doc.id),
-      ...doc.data(),
-    } as AchievementItem;
-    if (achievement.name.toLowerCase().includes(decodedQuery)) {
-      data.push(achievement);
-    }
-  });
+  if (error) {
+    throw new Error(error.message);
+  }
 
   return data;
 };
