78b507ebe5252ae2000c796fe017fcc17d30c19a
satria bahari <satriaabaharii@gmail.com>
2025-08-03 01:50:02 +0700

    fix: fix performance

--- common/components/elements/StackImages.tsx
+++ common/components/elements/StackImages.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useState } from "react";
+
+import Image from "./Image";
 
 interface CardRotateProps {
   children: React.ReactNode;
@@ -60,28 +61,7 @@ export default function Stack({
   animationConfig = { stiffness: 260, damping: 20 },
   sendToBackOnClick = false,
 }: StackProps) {
-  const [cards, setCards] = useState(
-    cardsData.length
-      ? cardsData
-      : [
-          {
-            id: 1,
-            img: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format",
-          },
-          {
-            id: 2,
-            img: "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=500&auto=format",
-          },
-          {
-            id: 3,
-            img: "https://images.unsplash.com/photo-1452626212852-811d58933cae?q=80&w=500&auto=format",
-          },
-          {
-            id: 4,
-            img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format",
-          },
-        ],
-  );
+  const [cards, setCards] = useState(cardsData);
 
   const sendToBack = (id: number) => {
     setCards((prev) => {
