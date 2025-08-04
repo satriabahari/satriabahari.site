41f16bc256122f6247783f693fd7530b66def9d1
satria bahari <satriaabaharii@gmail.com>
2025-08-04 17:09:09 +0700

    feat: add domain param in umami route API

--- app/api/umami/route.ts
+++ app/api/umami/route.ts
@@ -1,21 +1,47 @@
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { getPageViewsByDataRange, getWebsiteStats } from "@/services/umami";
+import { UMAMI_ACCOUNT } from "@/common/constants/umami";
 
-export const GET = async () => {
-  const pageViewsByDataRange = await getPageViewsByDataRange();
-  const websiteStats = await getWebsiteStats();
+export const GET = async (req: NextRequest) => {
+  const { websites } = UMAMI_ACCOUNT;
 
-  const result = {
-    ...pageViewsByDataRange.data,
-    websiteStats: websiteStats.data,
-  };
+  try {
+    let domain = req.nextUrl.searchParams.get("domain");
 
-  if (result.status >= 400) {
+    if (!domain) {
+      domain = websites?.[0]?.domain;
+      if (!domain) {
+        return NextResponse.json(
+          { message: "No available domain" },
+          { status: 400 },
+        );
+      }
+    }
+
+    const pageViews = await getPageViewsByDataRange(domain);
+    const stats = await getWebsiteStats(domain);
+
+    if (pageViews.status >= 400 || stats.status >= 400) {
+      return NextResponse.json(
+        {
+          message:
+            pageViews.error || stats.error || "Failed to fetch Umami data",
+        },
+        { status: pageViews.status || stats.status },
+      );
+    }
+
     return NextResponse.json(
-      { message: result.error || "Failed to fetch Umami data" },
-      { status: result.status },
+      {
+        ...pageViews.data,
+        websiteStats: stats.data,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
     );
   }
-
-  return NextResponse.json(result, { status: 200 });
 };
