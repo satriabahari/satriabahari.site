ede0e2366adde86bee84920c32c2726f55c27853
satria bahari <satriaabaharii@gmail.com>
2025-08-04 17:09:50 +0700

    feat: add websites property to UMAMI_ACCOUNT

--- common/constants/umami.ts
+++ common/constants/umami.ts
@@ -1,18 +1,30 @@
 export const UMAMI_ACCOUNT = {
   username: "Satria Bahari",
-  umami_url: "https://us.umami.is/share/wg6XA2bPFWg8Qc7r/www.satriabahari.site",
   api_key: process.env.UMAMI_API_KEY,
-  website_id: process.env.UMAMI_WEBSITE_ID,
+  base_url: "https://api.umami.is/v1/websites",
   endpoint: {
     page_views: "/pageviews",
     sessions: "/sessions/stats",
   },
-  base_url: "https://api.umami.is/v1/websites",
   parameters: {
-  startAt: 1717174800000, // 1 Juni 2024 00:00 WIB
+    startAt: 1717174800000, // 1 Juni 2024 00:00 WIB
     endAt: 1767190799000, // 31 Desember 2025 23:59 WIB
     unit: "month",
     timezone: "Asia/Jakarta",
   },
   is_active: true,
+  websites: [
+    {
+      domain: "satriabahari.site",
+      website_id: process.env.UMAMI_WEBSITE_ID_SITE,
+      umami_url:
+        "https://us.umami.is/share/wg6XA2bPFWg8Qc7r/www.satriabahari.site",
+    },
+    {
+      domain: "satriabahari.my.id",
+      website_id: process.env.UMAMI_WEBSITE_ID_MYID,
+      umami_url:
+        "https://cloud.umami.is/share/YBbXz2wWG0lCgSLt/www.satriabahari.my.id",
+    },
+  ],
 };
